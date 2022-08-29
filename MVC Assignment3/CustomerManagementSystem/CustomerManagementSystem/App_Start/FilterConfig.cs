171d10506622028500b38450217cfef673c9e591
sowmya616 <chsowmya161@gmail.com>
2022-08-29 20:12:30 +0530

    first commit

---
+++ MVC Assignment3/CustomerManagementSystem/CustomerManagementSystem/App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomerManagementSystem.App_Start
+{
+    public class FilterConfig
+    {
+        public static void RegisteredGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
